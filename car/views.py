d6c7e88c9fd20d81d12ec8d2c7650f8242764136
Mohammad Emam Ghorashi <print.mohammad.gh@gmail.com>
2024-07-14 15:27:18 +0330

    delete root templates & add index.html file [location: car/templates/car/index.html]

--- car/views.py
+++ car/views.py
@@ -4,12 +4,5 @@ from django.views.generic import ListView
 from .models import Brand, Car
 
 def home_view(request):
-    cars = Car.objects.all()
-    brands = Brand.objects.all()
-    
-    context = {
-        'cars': cars,
-        'brands': brands
-    }
-
-    return render(request, 'car/home.html', context)
+    context = {}
+    return render(request, 'car/index.html', context)
--- config/settings.py
+++ config/settings.py
@@ -68,9 +68,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            str(BASE_DIR.joinpath('templates')),
-            ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
